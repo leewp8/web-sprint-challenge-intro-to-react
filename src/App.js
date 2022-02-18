8375fd35bace1d97f60202286b1a072372739313
William Lee <leewp8@gmail.com>
2022-02-18 02:04:25 -0500

    almost done with appjs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
 
 const App = () => {
@@ -8,10 +9,45 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([])
+  const [currentCharacterId, setCurrentCharacterId] = useState('0')
+  
+  const openDetails = id => {
+    setCurrentCharacterId(id)
+  }
+
+  const closeDetails = () => {
+    setCurrentCharacterId(null)
+  }
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(res => {
+        console.log(res.data)
+        setCharacters(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, []
+  
+  )
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">STAR WARS CHARACTERS</h1>
+      {
+        characters.map((char) => {
+          return <div
+                    key={char.id} 
+                    info={char} 
+                    action={openDetails} 
+                    />
+        })
+      }
+      {
+        currentCharacterId && <div characterId={currentCharacterId} close={closeDetails}/>
+      }
     </div>
   );
 }
